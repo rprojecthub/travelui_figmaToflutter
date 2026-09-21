f09ff2f533175603d2c8d4aaef8c3397f807210b
rprojecthub <rndesai97@gmail.com>
2026-09-21 21:41:00 +0530

    main.dart file added

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:travel_ui/screens/TravelScreen3.dart';
+import 'package:travel_ui/screens/Travelsample.dart';
+import 'package:travel_ui/screens/TravelScreen1.dart';
+import 'package:travel_ui/screens/TravelScreen2.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      /* theme: ThemeData(
+        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+      ),*/
+      home: TravelScreen1(),
+    );
+  }
+}
+
+
+
+
+
+
