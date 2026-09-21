523a58197ec8da94a38ca34f199aa3c4a2ec76db
rprojecthub <rndesai97@gmail.com>
2026-09-21 21:35:01 +0530

    place details added

---
+++ lib/models/place.dart
@@ -0,0 +1,13 @@
+class Place {
+  final String image;
+  final String title;
+  final String location;
+  final double rating;
+
+  const Place({
+    required this.image,
+    required this.title,
+    required this.location,
+    required this.rating,
+  });
+}
---
+++ lib/models/places_datalist.dart
@@ -0,0 +1,17 @@
+import 'package:travel_ui/constants/app_assets.dart';
+import 'package:travel_ui/models/place.dart';
+
+final List<Place> places = [
+  Place(
+      image: AppAssets.image1,
+      title: 'Mount Fuji, Tokyo',
+      location: 'Tokyo, Japan',
+      rating: 4.8,
+  ),
+  Place(
+    image: AppAssets.image2,
+    title: 'Andes Mountain',
+    location: 'South, America',
+    rating: 4.7,
+  ),
+];
